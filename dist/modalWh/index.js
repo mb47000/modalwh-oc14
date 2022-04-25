244ebace6e4f91212a02f9ee061f53edad235b9f
Michel Brousse <mbweb47@gmail.com>
2022-04-25 18:17:09 +0200

    🚀 major release on npm 1.0.0

--- dist/modalWh/index.js
+++ dist/modalWh/index.js
@@ -13,17 +13,21 @@ var _focusTrapReact = _interopRequireDefault(require("focus-trap-react"));
 
 var _react = _interopRequireWildcard(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * Close Modal if clicked on outside of the container
+ * @param {React.Component} ref The container of the modal
+ * @param {function} setModalState The function to update the modal state.
+ */
 var useOutsideClose = function useOutsideClose(ref, setModalState) {
   (0, _react.useEffect)(function () {
-    /**
-     * Close Modal if clicked on outside of the container
-     */
     function handleClickOutside(event) {
       if (ref.current && !ref.current.contains(event.target)) {
         setModalState(false);
@@ -38,6 +42,17 @@ var useOutsideClose = function useOutsideClose(ref, setModalState) {
     };
   }, [ref, setModalState]);
 };
+/**
+ * A simple React Modal component, easy to customize
+ * @component
+ * @param {any} children The content of the modal, put anything you want
+ * @param {boolean} modalState The state of the modal, true for show, false for hide.
+ * @param {function} setModalState The function to update the modal state.
+ * @param {Object} modalStyle Custom style css for the different part of the modal, object shape: {main: {}, container: {}, content: {}, customCloseButton: {}}.
+ * @param {HtmlElement|ReactComponent} customCloseButton If you want to pass a personalized React component for close button.
+ * @returns {React.Component} React Modal component
+ */
+
 
 var ModalWh = function ModalWh(_ref) {
   var children = _ref.children,
@@ -47,15 +62,33 @@ var ModalWh = function ModalWh(_ref) {
       customCloseButton = _ref.customCloseButton;
   var containerRef = (0, _react.useRef)(null);
   useOutsideClose(containerRef, setModalState);
-  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, modalState && /*#__PURE__*/_react.default.createElement(_style.StyledModal, null, /*#__PURE__*/_react.default.createElement(_focusTrapReact.default, null, /*#__PURE__*/_react.default.createElement(_style.StyledModalContainer, {
-    ref: containerRef
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, modalState && /*#__PURE__*/_react.default.createElement(_style.StyledModal, {
+    customStyle: modalStyle === null || modalStyle === void 0 ? void 0 : modalStyle.main
+  }, /*#__PURE__*/_react.default.createElement(_focusTrapReact.default, null, /*#__PURE__*/_react.default.createElement(_style.StyledModalContainer, {
+    ref: containerRef,
+    customStyle: modalStyle === null || modalStyle === void 0 ? void 0 : modalStyle.container
   }, customCloseButton || /*#__PURE__*/_react.default.createElement(_style.StyledCloseButton, {
     onClick: function onClick() {
       return setModalState(false);
     },
-    tabIndex: "0"
-  }), /*#__PURE__*/_react.default.createElement(_style.StyledModalContent, null, children)))));
+    tabIndex: "0",
+    customStyle: modalStyle === null || modalStyle === void 0 ? void 0 : modalStyle.closeButton
+  }), /*#__PURE__*/_react.default.createElement(_style.StyledModalContent, {
+    customStyle: modalStyle === null || modalStyle === void 0 ? void 0 : modalStyle.content
+  }, children)))));
 };
 
+ModalWh.propTypes = {
+  modalState: _propTypes.default.bool.isRequired,
+  setModalState: _propTypes.default.func.isRequired,
+  customCloseButton: _propTypes.default.element,
+  modalStyle: _propTypes.default.shape({
+    main: _propTypes.default.string,
+    container: _propTypes.default.string,
+    content: _propTypes.default.string,
+    closeButton: _propTypes.default.string
+  }),
+  children: _propTypes.default.any
+};
 var _default = ModalWh;
 exports.default = _default;
--- dist/modalWh/style.js
+++ dist/modalWh/style.js
@@ -13,34 +13,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var StyledModal = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  position: fixed;\n  top: 0;\n  left: 0;\n  width: 100%;\n  height: 100vh;\n  background: ", ";\n  display: flex;\n  justify-content: center;\n  align-items: center;\n"])), function (props) {
-  return props.background || 'rgba(0, 0, 0, 0.75)';
+var StyledModal = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  position: fixed;\n  top: 0;\n  left: 0;\n  width: 100%;\n  height: 100vh;\n  background: rgba(0, 0, 0, 0.75);\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  ", ";\n"])), function (props) {
+  return props.customStyle;
 });
 
 exports.StyledModal = StyledModal;
 
-var StyledModalContainer = _styledComponents.default.div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  background: ", ";\n  border-radius: ", ";\n  width: ", ";\n  max-width: ", ";\n  padding: ", ";\n  position: relative;\n"])), function (props) {
-  return props.background || 'rgb(255, 255, 255)';
-}, function (props) {
-  return props.borderRadius || '5px';
-}, function (props) {
-  return props.width || '90%';
-}, function (props) {
-  return props.maxWidth || '400px';
-}, function (props) {
-  return props.padding || '38px';
+var StyledModalContainer = _styledComponents.default.div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  background: rgb(255, 255, 255);\n  border-radius: 5px;\n  width: 90%;\n  max-width: 400px;\n  padding: 38px;\n  position: relative;\n  ", ";\n"])), function (props) {
+  return props.customStyle;
 });
 
 exports.StyledModalContainer = StyledModalContainer;
 
-var StyledModalContent = _styledComponents.default.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  text-align: ", ";\n  background: ", ";\n"])), function (props) {
-  return props.textAlign || 'center';
-}, function (props) {
-  return props.background || 'inherit';
+var StyledModalContent = _styledComponents.default.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  text-align: center;\n  background: inherit;\n  ", ";\n"])), function (props) {
+  return props.customStyle;
 });
 
 exports.StyledModalContent = StyledModalContent;
 
-var StyledCloseButton = _styledComponents.default.a(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n  position: absolute;\n  right: 5px;\n  top: 5px;\n  width: 32px;\n  height: 32px;\n  opacity: 0.3;\n\n  &:hover {\n    opacity: 1;\n    cursor: pointer;\n  }\n\n  &:before,\n  &:after {\n    position: absolute;\n    content: ' ';\n    height: 33px;\n    width: 2px;\n    left: 14px;\n    background-color: #333;\n  }\n\n  &:before {\n    transform: rotate(45deg);\n  }\n  &:after {\n    transform: rotate(-45deg);\n  }\n"])));
+var StyledCloseButton = _styledComponents.default.a(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n  position: absolute;\n  right: 5px;\n  top: 5px;\n  width: 32px;\n  height: 32px;\n  opacity: 0.3;\n\n  &:hover {\n    opacity: 1;\n    cursor: pointer;\n  }\n\n  &:before,\n  &:after {\n    position: absolute;\n    content: ' ';\n    height: 33px;\n    width: 2px;\n    left: 14px;\n    background-color: #333;\n  }\n\n  &:before {\n    transform: rotate(45deg);\n  }\n  &:after {\n    transform: rotate(-45deg);\n  }\n\n  ", ";\n"])), function (props) {
+  return props.customStyle;
+});
 
 exports.StyledCloseButton = StyledCloseButton;
